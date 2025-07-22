931d9e0f6d10b8f490c7fddfdb7857128f511837
Dia Patel <Dia.Patel@horizontal.com>
2025-07-22 11:24:54 +0530

    commit dll

--- MoviesHub/MoviesHub/obj/Debug/net8.0/MoviesHub.AssemblyInfo.cs
+++ MoviesHub/MoviesHub/obj/Debug/net8.0/MoviesHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7c170b6627a2be81e4e3ee1f72e573d904d5e6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7bae558fe23505efcd296a0c3a00a98ef7bc97b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
